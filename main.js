673190af10ce0f5d8f23dbc5af3b8ab4ea7b98b2
wzzzzzz <969188976@qq.com>
2019-08-16 23:20:53 +0800

    Signed-off-by: wzzzzzz <969188976@qq.com>

---
+++ main.js
@@ -0,0 +1,50 @@
+var num=[
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0]
+];
+
+var init=function(){
+    //初始化，随机两个位置出现2
+
+}
+
+var check=function(){
+    //检查游戏是否输掉
+    
+}
+
+var up=function(){
+    //键盘上，每一列往上移，并向上加，加的时候检查，检查完渲染
+
+    show();
+}
+var down=function(){
+    //键盘下，每一列往下移，并向下加，加的时候检查，检查完渲染
+    
+    show();
+}
+var left=function(){
+    //键盘左，每一行左移，并向左加，加的时候检查，检查完渲染
+
+    show();
+}
+var right=function(){
+    //键盘右，每一行右移，并向右加，加的时候检查，检查完渲染
+
+    show();
+}
+
+var show=function(){
+    //更新渲染
+
+}
+
+var win=function(){
+
+}
+
+var lose=function(){
+    
+}
